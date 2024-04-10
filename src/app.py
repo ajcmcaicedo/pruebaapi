7494c694f6519fcb95dbc2beba5e22524f14a4aa
ajcmcaicedo <121562135+ajcmcaicedo@users.noreply.github.com>
2024-04-09 22:00:50 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,63 +5,59 @@ import database as db
 template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 template_dir = os.path.join(template_dir, 'src', 'templates')
 
-def crear_app():
+app = Flask(__name__, template_folder = template_dir)
 
-    app = Flask(__name__, template_folder = template_dir)
+#Rutas de la aplicación
+@app.route('/')
+def home():
+    cursor = db.database.cursor()
+    cursor.execute("SELECT * FROM usuarios")
+    myresult = cursor.fetchall()
+    #Convertir los datos a diccionario
+    insertObject = []
+    columnNames = [column[0] for column in cursor.description]
+    for record in myresult:
+        insertObject.append(dict(zip(columnNames, record)))
+    cursor.close()
+    return render_template('index.html', data=insertObject)
 
-    #Rutas de la aplicación
-    @app.route('/')
-    def home():
+#Ruta para guardar usuarios en la bdd
+@app.route('/user', methods=['POST'])
+def addUser():
+    username = request.form['username']
+    name = request.form['name']
+    password = request.form['password']
+
+    if username and name and password:
         cursor = db.database.cursor()
-        cursor.execute("SELECT * FROM usuarios")
-        myresult = cursor.fetchall()
-        #Convertir los datos a diccionario
-        insertObject = []
-        columnNames = [column[0] for column in cursor.description]
-        for record in myresult:
-            insertObject.append(dict(zip(columnNames, record)))
-        cursor.close()
-        return render_template('index.html', data=insertObject)
-
-    #Ruta para guardar usuarios en la bdd
-    @app.route('/user', methods=['POST'])
-    def addUser():
-        username = request.form['username']
-        name = request.form['name']
-        password = request.form['password']
-
-        if username and name and password:
-            cursor = db.database.cursor()
-            sql = "INSERT INTO usuarios (username, name, password) VALUES (%s, %s, %s)"
-            data = (username, name, password)
-            cursor.execute(sql, data)
-            db.database.commit()
-        return redirect(url_for('home'))
-
-    @app.route('/delete/<string:id>')
-    def delete(id):
-        cursor = db.database.cursor()
-        sql = "DELETE FROM usuarios WHERE id=%s"
-        data = (id,)
+        sql = "INSERT INTO usuarios (username, name, password) VALUES (%s, %s, %s)"
+        data = (username, name, password)
         cursor.execute(sql, data)
         db.database.commit()
-        return redirect(url_for('home'))
+    return redirect(url_for('home'))
 
-    @app.route('/edit/<string:id>', methods=['POST'])
-    def edit(id):
-        username = request.form['username']
-        name = request.form['name']
-        password = request.form['password']
+@app.route('/delete/<string:id>')
+def delete(id):
+    cursor = db.database.cursor()
+    sql = "DELETE FROM usuarios WHERE id=%s"
+    data = (id,)
+    cursor.execute(sql, data)
+    db.database.commit()
+    return redirect(url_for('home'))
 
-        if username and name and password:
-            cursor = db.database.cursor()
-            sql = "UPDATE usuarios SET username = %s, name = %s, password = %s WHERE id = %s"
-            data = (username, name, password, id)
-            cursor.execute(sql, data)
-            db.database.commit()
-        return redirect(url_for('home'))
-    return app
+@app.route('/edit/<string:id>', methods=['POST'])
+def edit(id):
+    username = request.form['username']
+    name = request.form['name']
+    password = request.form['password']
+
+    if username and name and password:
+        cursor = db.database.cursor()
+        sql = "UPDATE usuarios SET username = %s, name = %s, password = %s WHERE id = %s"
+        data = (username, name, password, id)
+        cursor.execute(sql, data)
+        db.database.commit()
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app = crear_app
     app.run(debug=True, port=5000)
